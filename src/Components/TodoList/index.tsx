b2dac906bd72768c289012647b1fd70631c5fc3c
leejiwon6315 <leejiwon6315@naver.com>
2021-02-09 23:02:57 +0900

    todo list styling

--- src/Components/TodoList/index.tsx
+++ src/Components/TodoList/index.tsx
@@ -2,7 +2,10 @@ import style from "./TodoList.module.scss";
 const TodoList: React.FC = () => {
   return (
     <ul className={style.list_wrapper}>
-      <li className={style.list}>todo</li>
+      <li className={style.list}>
+        <p className={style.todo}>todo</p>
+        <p className={style.delete_todo}>×</p>
+      </li>
     </ul>
   );
 };
